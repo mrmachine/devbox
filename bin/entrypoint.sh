20593affd29191a3ef97d8ef0deb7f91c41acc76
Tai Lee <real.human@mrmachine.net>
2017-07-27 00:18:15 +1000

    Use trust authentication for all local connections. Use existing `postgres` user by default.

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -2,11 +2,12 @@
 
 set -e
 
+# Configure PostgreSQL.
+echo 'local all all trust' > '/etc/postgresql/9.5/main/pg_hba.conf'
+export PGUSER=postgres
+
 /etc/init.d/elasticsearch start
 /etc/init.d/postgresql start
 /etc/init.d/redis-server start
-
-# Create PostgreSQL superuser.
-su - postgres -c 'createuser -s root' || true
 
 exec "$@"
