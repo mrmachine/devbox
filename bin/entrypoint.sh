240a3247ca86f7bdcb314c36fa12eff0919b825b
Tai Lee <real.human@mrmachine.net>
2017-07-27 01:02:51 +1000

    Enable trust authentication for all connections.

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-# Configure PostgreSQL.
-echo 'local all all trust' > '/etc/postgresql/9.5/main/pg_hba.conf'
+# Enable trust authentication for all connections.
+cat <<EOF > /etc/postgresql/9.5/main/pg_hba.conf
+local all all     trust
+host  all all all trust
+EOF
 
 /etc/init.d/elasticsearch start
 /etc/init.d/postgresql start
